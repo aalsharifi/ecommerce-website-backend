c8eda5f81dfecaec1e989279c9ab120c0c0f4b7e
aalsharifi <94931589+aalsharifi@users.noreply.github.com>
2022-01-02 10:50:02 -0500

    decided to not include the delete option

--- src/main/java/com/ecommerce/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/ProductController.java
@@ -57,13 +57,13 @@ public class ProductController {
         return new ResponseEntity<>(new ApiResponse(true, "Product has been updated!"), HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete/{productId}")
-    public ResponseEntity<ApiResponse> deleteProduct(@PathVariable("productId") Integer productId){
-
-        productService.deleteProduct(productId);
-
-        return new ResponseEntity<>(new ApiResponse(true, "Successfully deleted product "), HttpStatus.OK);
-    }
+//    @DeleteMapping("/delete/{productId}")
+//    public ResponseEntity<ApiResponse> deleteProduct(@PathVariable("productId") Integer productId){
+//
+//        productService.deleteProduct(productId);
+//
+//        return new ResponseEntity<>(new ApiResponse(true, "Successfully deleted product "), HttpStatus.OK);
+//    }
 
 
 
